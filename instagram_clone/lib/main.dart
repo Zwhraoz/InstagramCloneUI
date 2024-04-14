0fc29a15522b945562b2bc945f4cbbf0dbd6db33
Zehra Özer <Ozerzwhra@gmail.com>
2024-04-14 15:43:21 +0300

    clone

---
+++ instagram_clone/lib/main.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<String> posts = [
+    'https://fotolifeakademi.com/uploads/2020/04/manzara-fotografi-cekmek-724x394.webp',
+    'https://cdn.pixabay.com/photo/2016/02/10/21/59/landscape-1192669_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/03/07/14/53/sunset-2124191_1280.jpg',
+  ];
+
+  final List<String> stories = List.generate(
+    25,
+    (index) => 'https://source.unsplash.com/random/60x60?sig=${index}',
+  );
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Instagram Clone',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Billabong',
+            fontSize: 28.0,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {
+              // Navigate to search page
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.chat_sharp, color: Colors.white),
+            onPressed: () {
+              // Navigate to notifications page
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: stories.length + 1, // Mavi artı için +1
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  // Profil resmi
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.white, // Profil rengi
+                              width: 1,
+                            ),
+                          ),
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage: NetworkImage(stories[index]),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          right: 5,
+                          child: Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  // Diğer hikayeler
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: index < 5
+                              ? Colors.pink
+                              : const Color.fromARGB(255, 141, 136, 136)!,
+                          width: 3,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(stories[index - 1]),
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                     backgroundImage: NetworkImage('https://a.storyblok.com/f/191576/1200x800/215e59568f/round_profil_picture_after_.webp'),
+                          ),
+                      title: Text(
+                        'zehraozer',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      subtitle: Text('Istanbul',
+                          style: TextStyle(color: Colors.white)),
+                      trailing: Icon(Icons.more_vert, color: Colors.white),
+                    ),
+                    Image.network(
+                      posts[index],
+                      fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              IconButton(
+                                icon: Icon(Icons.favorite_border,
+                                    size: 28, color: Colors.white),
+                                onPressed: () {
+                                  Colors.pink;
+                                  print("beğen tıklandı");
+                                },
+                              ),
+                              SizedBox(width: 8.0),
+                              IconButton(
+                                icon: Icon(Icons.comment,
+                                    size: 28, color: Colors.white),
+                                onPressed: () {
+                                  // Navigate to comments page
+                                },
+                              ),
+                              SizedBox(width: 8.0),
+                              IconButton(
+                                icon: Icon(Icons.send,
+                                    size: 28, color: Colors.white),
+                                onPressed: () {
+                                  // Handle send button tap
+                                },
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.bookmark_border,
+                                size: 28, color: Colors.white),
+                            onPressed: () {
+                              // Handle bookmark button tap
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Text(
+                        '30 beğenme',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text('zehraozer: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          Text('Açıklama',
+                              style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Color.fromARGB(255, 220, 213, 213),
+       
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box),
+            label: 'Post',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Likes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
